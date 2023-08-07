3f7db40de377132d16681d1da010f832439edad9
Nestor Nunes <109300513+netinho-RB@users.noreply.github.com>
2023-08-07 15:57:46 -0300

    upfiles

---
+++ public/js/script.js
@@ -0,0 +1,17 @@
+// Função para registro de vendas
+function registrarVenda() {
+    // Adicione o código para registrar vendas aqui
+  }
+  
+  // Função para processar o formulário de contato
+  function processarFormularioContato(event) {
+    event.preventDefault();
+    // Adicione o código para processar o formulário de contato aqui
+  }
+  
+  // Event Listener para o formulário de contato
+  const formularioContato = document.getElementById('formulario-contato');
+  formularioContato.addEventListener('submit', processarFormularioContato);
+  
+  // Outras funções e lógica JavaScript do seu projeto
+  
